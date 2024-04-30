b32b54eeaf4b44d385b6b56efdc0b8e24eac9398
Nathan Marsh <nwmarsh1992@gmail.com>
2024-04-30 10:14:24 -0400

    fails

--- packages/caching/src/index.ts
+++ packages/caching/src/index.ts
@@ -2,5 +2,5 @@ export interface Sample {
   name: string;
 }
 export const greet = (name: Sample): string => {
-  return `Hello from caching, ${name.name}, change here, some more, so close!`;
+  return `Hello from caching, ${name.name}, change here, some more, so close, this probably fails!`;
 };
